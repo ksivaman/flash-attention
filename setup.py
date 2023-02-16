ebc7b9e41e629ae19b83b7b8f557c5925ff7b2b4
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2023-02-16 22:45:34 +0000

    PEP 440 compliant canonical version
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ ext_modules.append(
 
 setup(
     name="flash_attn",
-    version="0.2.8_hopper",
+    version="0.2.8.dev0",
     packages=find_packages(
         exclude=("build", "csrc", "include", "tests", "dist", "docs", "benchmarks", "flash_attn.egg-info",)
     ),
